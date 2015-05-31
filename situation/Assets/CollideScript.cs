40db807abcc3a5133390d6c05e84069dad4b0401
Gidtmh <gideontmh@gmail.com>
2015-05-31 20:53:16 +1000

    Collectible Resource Created

---
+++ situation/Assets/CollideScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+        void OnTriggerEnter(Collider player) {
+
+		if(player.gameObject.tag == "player")
+		{
+			this.gameObject.SetActive(false);
+
+		}
+        }
+	
+	
+	}
+
---
+++ situation/Assets/collisionscript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
